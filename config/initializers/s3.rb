f4e97744f41cde1ea515d77b65bbf648ca10d44f
jesontheg6 <jesonleejunzhen@gmail.com>
2017-11-24 19:51:12 +0800

    fix my heroku 2

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -5,8 +5,8 @@ CarrierWave.configure do |config|
 
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
-    :aws_access_key_id      => 'ENV['S3_KEY']',                        # required
-    :aws_secret_access_key  => 'ENV['S3_SECRET_KEY']',                     # required
+    :aws_access_key_id      => ENV['S3_KEY'],                        # required
+    :aws_secret_access_key  => ENV['S3_SECRET_KEY'],                     # required
     :region                 => 'ap-southeast-1',                  # 
 
   config.fog_directory  = 'pooptimer'                             # required
