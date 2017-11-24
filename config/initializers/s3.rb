0bcc8e3e7ac6f9082f112ca0a699c1d85aa0025f
jesontheg6 <jesonleejunzhen@gmail.com>
2017-11-25 00:59:23 +0800

    solve conflict

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,5 +1,6 @@
 # config/initializers/carrierwave.rb
 CarrierWave.configure do |config|
+	config.fog_provider = 'fog/aws'
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
     :aws_access_key_id      => ENV['S3_KEY'],                        # required
