c3a0cdc9392ce69bf6f96c2d79abcce30725c440
jesontheg6 <jesonleejunzhen@gmail.com>
2017-11-24 20:01:27 +0800

    resolve conflict

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -4,7 +4,9 @@ CarrierWave.configure do |config|
     :provider               => 'AWS',                        # required
     :aws_access_key_id      => ENV['S3_KEY'],                        # required
     :aws_secret_access_key  => ENV['S3_SECRET_KEY'],                     # required
-    :region                 => 'ap-southeast-1',                  # 
+    :region                 => 'ap-southeast-1',
+
+    }                  # 
 
   config.fog_directory  = 'pooptimer'                             # required
  
